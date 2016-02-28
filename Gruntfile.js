6c1428c79dafd300969e1e379f0146f7e0f7e5a9
Ivan Rybalka <Vano@MBP-Ivan.Dlink>
2016-02-28 15:32:10 +0200

    Configured static HTML complication with Jade

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		jade: {
+			compile: {
+				options: {
+					pretty: true
+				},
+				files: {
+					'index.html': 'views/pages/index.jade',
+					'projects.html': 'views/pages/projects.jade',
+					'contacts.html': 'views/pages/contacts.jade'
+				}
+			}
+		}
+	})
+
+	grunt.loadNpmTasks('grunt-contrib-jade');
+
+	grunt.registerTask('default', ['jade']);
+
+};
